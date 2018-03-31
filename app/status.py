7e38af0441b9f9c412babe1d6719d0c61705de50
t-bence <toth.bence.mihaly@gmail.com>
2018-03-31 14:56:26 +0200

    Added more observed parameters

--- app/status.py
+++ app/status.py
@@ -9,13 +9,35 @@ def get_system_data():
     CPU_temp = getoutput('vcgencmd measure_temp').split("=")[1].split("'")[0]
     core_volts = getoutput('vcgencmd measure_volts core').split("=")[1][0:-3:]
     core_hertz = str(int(getoutput('vcgencmd measure_clock arm').split("=")[1])/1e6)
+    uptime = getoutput('uptime -s')
+    kernel = getoutput('uname -r')
+    hostname = getoutput('hostname')
+    total_mem = str(round(int(getoutput('cat /proc/meminfo | grep "MemTotal" | egrep "[0-9.]{4,}" -o'))/1024))
+    free_mem = str(round(int(getoutput('cat /proc/meminfo | grep "MemFree" | egrep "[0-9.]{4,}" -o'))/1024))
+    avail_mem = str(round(int(getoutput('cat /proc/meminfo | grep "MemAvailable" | egrep "[0-9.]{4,}" -o'))/1024.0))
+    space = getoutput('df -h /home/pi/hdd').split()
+    total_space = space[8][0:-1] + ' ' + space[8][-1]
+    used_space = space[9][0:-1] + ' ' + space[9][-1]
+    free_space = space[10][0:-1] + ' ' + space[10][-1]
+    percent_space = space[11]
+    percent_cpu = getoutput("top -d 0.5 -b -n2 | grep 'Cpu(s)'|tail -n 1 | awk '{print $2 + $4}'")
 
     data.append(('LAN download', LAN_download))
     data.append(('CPU temperature', CPU_temp + ' &deg;C'))
+    data.append(('CPU usage', percent_cpu + '%'))
     data.append(('Core voltage', core_volts + ' V'))
     data.append(('Core frequency', core_hertz + ' MHz'))
+    data.append(('Running since', uptime))
+    data.append(('Kernel version', kernel))
+    data.append(('Hostname', hostname))
+    data.append(('Total memory', total_mem + ' M'))
+    data.append(('Free memory', free_mem + ' M'))
+    data.append(('Available memory', avail_mem + ' M'))
+    data.append(('Total HDD space', total_space))
+    data.append(('Used HDD space', used_space + ' (' + percent_space + ')'))
+    data.append(('Free HDD space', free_space))
     
-    
+
     return data
     
 def get_mark_my_prof_data():
