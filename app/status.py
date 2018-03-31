c963c2e4517ad84f0428e785e886d1bbc5ad19f4
t-bence <toth.bence.mihaly@gmail.com>
2018-03-31 15:02:55 +0200

    Avoid an error with ifconfig results

--- app/status.py
+++ app/status.py
@@ -5,7 +5,10 @@ from subprocess import getoutput
 
 def get_system_data():
     data = []
-    LAN_download = getoutput('ifconfig | grep "RX packets"').split("\n")[0].split("(")[1].split(")")[0]
+    try:
+        LAN_download = getoutput('ifconfig | grep "RX packets"').split("\n")[0].split("(")[1].split(")")[0]
+    except:
+        LAN_download = "0"
     CPU_temp = getoutput('vcgencmd measure_temp').split("=")[1].split("'")[0]
     core_volts = getoutput('vcgencmd measure_volts core').split("=")[1][0:-3:]
     core_hertz = str(int(getoutput('vcgencmd measure_clock arm').split("=")[1])/1e6)
