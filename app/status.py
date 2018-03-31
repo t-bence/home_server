cffc70808022ee4e232d230d3e8e7cdab4fa37b3
t-bence <toth.bence.mihaly@gmail.com>
2018-03-31 11:29:30 +0200

    Made status page work again

--- app/status.py
+++ app/status.py
@@ -1,18 +1,14 @@
 from os import path
 from datetime import datetime
-#from commands import getstatusoutput
+from subprocess import getoutput
 
 
 def get_system_data():
     data = []
-#    LAN_download = getstatusoutput('ifconfig | grep "RX packets"')[1].split("\n")[0].split("(")[1].split("0")[0][0:-1:]
-#    CPU_temp = getstatusoutput('vcgencmd measure_temp')[1].split("=")[1].split("'")[0]
-#    core_volts = getstatusoutput('vcgencmd measure_volts core')[1].split("=")[1][0:-3:]
-#    core_hertz = str(int(getstatusoutput('vcgencmd measure_clock arm')[1].split("=")[1])/1e6)
-    LAN_download = "0"
-    CPU_temp = "0"
-    core_volts = "0"
-    core_hertz = "0"
+    LAN_download = getoutput('ifconfig | grep "RX packets"').split("\n")[0].split("(")[1].split(")")[0]
+    CPU_temp = getoutput('vcgencmd measure_temp').split("=")[1].split("'")[0]
+    core_volts = getoutput('vcgencmd measure_volts core').split("=")[1][0:-3:]
+    core_hertz = str(int(getoutput('vcgencmd measure_clock arm').split("=")[1])/1e6)
 
     data.append(('LAN download', LAN_download))
     data.append(('CPU temperature', CPU_temp + ' C'))
